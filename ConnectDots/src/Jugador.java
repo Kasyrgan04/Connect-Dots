c5eee1d92005e4b45e7582e6322482db4387f386
Kasyrgan04 <dyguerrero@estudiantec.cr>
2023-09-08 13:40:52 -0600

    Ventana Juego

--- ConnectDots/src/Jugador.java
+++ ConnectDots/src/Jugador.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.event.*;
 import java.io.*;
 
 import javax.imageio.ImageIO;
@@ -7,7 +8,7 @@ public class Jugador {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Ventana ventana1=new Ventana();
+		VentanaInicio ventana1=new VentanaInicio();
 		
 		ventana1.setVisible(true);
 		ventana1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -15,9 +16,9 @@ public class Jugador {
 	}
 
 }
-class Ventana extends JFrame{
-	public Ventana() {
-		setBounds(500,300,400,400);
+class VentanaInicio extends JFrame{
+	public VentanaInicio() {
+		setExtendedState(Frame.MAXIMIZED_BOTH);
 		setTitle("Connect Dots");
 		//reconoce el medio en el cual se ejecuta la pantalla
 		Toolkit pantalla=Toolkit.getDefaultToolkit();
@@ -29,17 +30,58 @@ class Ventana extends JFrame{
 		lamina1.setBackground(new Color(0,28,115));
 	}
 }
+
+class VentanaJuego extends JFrame{
+	public VentanaJuego() {
+		setExtendedState(Frame.MAXIMIZED_BOTH);
+		setTitle("Connect Dots");
+		//reconoce el medio en el cual se ejecuta la pantalla
+		Toolkit pantalla=Toolkit.getDefaultToolkit();
+		//Cambia el icono predeterminado de la ventana
+		Image Icono=pantalla.getImage("src/Connect Dots.png");
+		setIconImage(Icono);
+	}
+}
+
 class Lamina extends JPanel{
 	private Image logo;
+	private JButton inicio,cierre;
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
-		
 		try {
 			logo=ImageIO.read(new File("src/Inicial.png"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		g.drawImage(logo, 500, 0, null);
+		
+		
+		
 	}
+	public Lamina() {
+		setLayout(null);
+		inicio=new JButton("Empezar"); 
+		cierre=new JButton("Salir");
+		inicio.setBounds(720, 600, 100, 40);
+		cierre.setBounds(720, 650, 100, 40);
+		//inicio.setBackground(new Color(253,230,53).brighter().brighter().brighter());
+		Inicio launch=new Inicio();
+		add(inicio);
+		add(cierre);
+		inicio.addActionListener(launch);
+		
+		
+	}
+	private class Inicio implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e)  {
+			// TODO Auto-generated method stub
+			VentanaJuego ventana2=new VentanaJuego();
+			ventana2.setVisible(true);
+			//dispose();
+		}
+		
+	}
+	
 }
